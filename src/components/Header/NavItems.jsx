322ac650985c00567885ec6e0adb32d03079aed9
devopati <davidopati70@gmail.com>
2023-03-04 10:06:41 +0300

    n

---
+++ src/components/Header/NavItems.jsx
@@ -0,0 +1,66 @@
+export const navItems = [
+  {
+    title: "Home",
+    path: "/",
+    cName: "nav-item",
+    linkClass: "nav-links",
+    iconClass: "",
+  },
+  {
+    title: "Our Expertise",
+    path: "/expertise",
+    cName: "nav-item",
+    linkClass: "nav-links",
+    iconClass: "fas fa-caret-down",
+  },
+  {
+    title: "Case Studies",
+    path: "/casestudy",
+    cName: "nav-item",
+    linkClass: "nav-links",
+    iconClass: "",
+  },
+
+  {
+    title: "About Us",
+    path: "/about",
+    cName: "nav-item",
+    linkClass: "nav-links",
+    iconClass: "",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    cName: "nav-item",
+    linkClass: "nav-links",
+    iconClass: "",
+  },
+];
+
+export const serviceDropdown = [
+  {
+    title: "Commercial",
+    path: "/commercial",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Residential",
+    path: "/residential",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Leisure",
+    path: "/leisure",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Work Offices",
+    path: "/about",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Shopping Malls",
+    path: "/about",
+    cName: "dropdown-link",
+  },
+];
